3e23d0406cd6fe65b15c2a9a77da5de3c0ebc8e2
Joon Kang <joonkang@monzo.com>
2023-12-28 15:25:03 +0000

    feat: catch runtime error

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 )
@@ -12,12 +13,17 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		app.serverError(w, r, err)
 		return
 	}
-	w.WriteHeader(status)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
+		return
 	}
+
+	w.WriteHeader(status)
+	buf.WriteTo(w)
 }
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
