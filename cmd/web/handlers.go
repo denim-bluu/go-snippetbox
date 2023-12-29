4aaef8d975548a01e25a2b7e15961da32a3889a2
Joon Kang <joonkang@monzo.com>
2023-12-29 14:45:33 +0000

    feat: Add create feature

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -43,20 +43,30 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprint("Ww")))
+	data := app.newTemplateData(r)
+	app.render(w, r, http.StatusOK, "create.html", data)
 }
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
-	fmt.Println(title)
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.notFound(w)
+		return
+	}
+
 	snippet, err := app.snippetModel.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-	w.Write([]byte(fmt.Sprint(snippet)))
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", snippet.ID), http.StatusSeeOther)
 }
 
 func (app *application) snippetDelete(w http.ResponseWriter, r *http.Request) {
