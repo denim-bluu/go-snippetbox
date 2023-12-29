84d75c10674fb6b0da07dedb30bd0e5e0cf777b2
Joon Kang <joonkang@monzo.com>
2023-12-29 11:26:25 +0000

    feat: Adding CSP headers

--- cmd/web/router.go
+++ cmd/web/router.go
@@ -8,9 +8,10 @@ import (
 
 func (app *application) newRouter() *mux.Router {
 	router := mux.NewRouter()
+	router.Use(secureHeaders)
 	s := http.StripPrefix("/static/", http.FileServer(http.Dir("./ui/static")))
 	router.PathPrefix("/static/").Handler(s)
-	router.HandleFunc("/", app.home).Methods(http.MethodGet)
+	router.HandleFunc("/", app.home).Methods(http.MethodGet, http.MethodHead)
 	router.HandleFunc("/snippet/view/{id}", app.snippetView).Methods(http.MethodGet)
 	router.HandleFunc("/snippet/create", app.snippetCreate).Methods(http.MethodPost)
 	router.HandleFunc("/snippet/delete/{id}", app.snippetDelete).Methods(http.MethodDelete)
